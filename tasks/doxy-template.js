b8805acb287c4a26d959b5ea11576b0e7ace215f
Stefano Pogliani <stefano@spogliani.net>
2015-01-18 16:14:16 +0000

    Started dealing with table of contents.

--- tasks/doxy-template.js
+++ tasks/doxy-template.js
@@ -10,15 +10,21 @@ module.exports = function(grunt) {
    * @param {!Object}    manifest    The parsed index.xml.
    * @param {!XmlParser} parser      The parser instance already created.
    * @param {!Function}  name        Build the output file name of a page.
+   * @param {Object}     toc         Table of contents.
    */
-  var parsePages = function parsePages(doxy_source, manifest, parser, name) {
+  var parsePages = function parsePages(
+      doxy_source, manifest, parser, name, toc
+  ) {
     var destinations = [];
     var pages   = manifest["page"];
     var results = [];
 
     for (var idx = 0; idx < pages.length; idx++) {
       parser.open(doxy_source, pages[idx].id + ".xml");
-      results.push(parser.parseAsPage());
+      var page = parser.parseAsPage();
+      if (toc) { page.toc = toc; }
+
+      results.push(page);
       destinations.push(name(pages[idx])); 
     }
     return {
@@ -26,6 +32,43 @@ module.exports = function(grunt) {
       dest: destinations,
       type: "multi-raw"
     };
+  };
+
+  /*!
+   * Produce a table of contents from a manifest.
+   * @param {!Object} manifest The manifest to process.
+   */
+  var tocFromManifest = function tocFromManifest(manifest) {
+    var toc = {};
+
+    // Reformat pages.
+    if ("page" in manifest) {
+      var pages = manifest.page;
+      toc.pages = [];
+      for (var idx = 0; idx < pages.length; idx++) {
+        var page = pages[idx];
+        toc.pages.push({
+          id:    page.id,
+          title: page.title
+        });
+
+        if (page.id === "indexpage") {
+          toc.index = page.title;
+        }
+      }
+
+      toc.pages = toc.pages.sort(function(left, right) {
+        if (left.title < right.title) {
+          return -1;
+        } else if (left.title == right.title) {
+          return 0;
+        } else {
+          return 1;
+        }
+      });
+    }
+
+    return toc;
   };
 
 
@@ -47,8 +90,9 @@ module.exports = function(grunt) {
 
     var doxy_dest   = this.files[0].dest;
     var doxy_source = this.files[0].src[0];
-    //var options     = this.options({
-    //});
+    var options     = this.options({
+      toc: true
+    });
 
     if (!grunt.file.isDir(doxy_source)) {
       grunt.fail.warn(
@@ -66,11 +110,23 @@ module.exports = function(grunt) {
     var data     = {};
     var manifest = {};
     var parser   = new XmlParser();
+    var toc      = null;
     parser.open(doxy_source, "index.xml");
     manifest = parser.parseAsManifest();
+    if (options.toc) {
+      toc = tocFromManifest(manifest);
+    }
 
     // Process manifest.
-    data["page"]    = parsePages(doxy_source, manifest, parser, namer);
+    data["page"] = parsePages(doxy_source, manifest, parser, namer, toc);
+
+    if (options.toc) {
+      data["toc-pages"] = {
+        data: toc,
+        dest: path.join(doxy_dest, "toc", "pages.html"),
+        type: "raw"
+      }
+    }
 
     // Expand results.
     grunt.config("handlebars-expand.docs", data);
